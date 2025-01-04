538a8ca9adb5c1a17dd472e2f348b00a3f922948
ultraspicy <element.no.88@gmail.com>
2025-01-04 12:00:17 -0800

    Minor change on zkvm/entrypoint, use a,b,p,q as the operands

--- crates/zkvm/entrypoint/src/syscalls/addmul.rs
+++ crates/zkvm/entrypoint/src/syscalls/addmul.rs
@@ -1,22 +1,29 @@
 #[cfg(target_os = "zkvm")]
 use core::arch::asm;
 
-/// Uint256 multiplication operation.
+/// a*b + c*d operation.
 ///
 /// The result is written over the first input.
 #[allow(unused_variables)]
 #[no_mangle]
-pub extern "C" fn syscall_uint256_mul(x: *mut u32, y: *const u32) {
-    #[cfg(target_os = "zkvm")]
+// Raw pointers don't follow Rust's normal borrowing rules
+// They're always unsafe to dereference
+// explicit use *const vs *mut distinction in raw pointers
+// &mut i32 - References
+// *mut i32 - raw pointer
+pub extern "C" fn syscall_add_mul(x: *mut u32, y: *mut u32, p: *mut u32, q: *mut u32) {
+    #[cfg(target_os = "zkvm")] // only compile this syscall when the target OS is zkvm
     unsafe {
         asm!(
             "ecall",
-            in("t0") crate::syscalls::UINT256_MUL,
+            in("t0") crate::syscalls::ADDMUL,
             in("a0") x,
             in("a1") y,
+            in("a2") p,
+            in("a3") q
         );
     }
 
-    #[cfg(not(target_os = "zkvm"))]
+    #[cfg(not(target_os = "zkvm"))] 
     unreachable!()
 }
--- crates/zkvm/lib/src/lib.rs
+++ crates/zkvm/lib/src/lib.rs
@@ -145,6 +145,6 @@ extern "C" {
     /// Executes a BN254 Fp2 multiplication on the given inputs.
     pub fn syscall_bn254_fp2_mulmod(p: *mut u32, q: *const u32);
 
-    /// Executes a ADD_MUL
-    pub fn syscall_add_mul(p: *mut u32, q: *const u32);
+    /// Executes a ADD_MUL that compute a*b + c*d
+    pub fn syscall_add_mul(p: *mut u32, q: *mut u32, r: *mut u32, s: *mut u32);
 }
