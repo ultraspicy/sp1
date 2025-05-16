4a753fbcd1ba892c685c982f358b581d403b33c0
Jianfeng Guo <element.no.88@gmail.com>
2025-05-15 23:13:15 -0700

    Add missing syscall code

--- crates/core/executor/src/syscalls/code.rs
+++ crates/core/executor/src/syscalls/code.rs
@@ -203,6 +203,7 @@ impl SyscallCode {
             0x00_01_01_2C => SyscallCode::SECP256R1_ADD,
             0x00_00_01_2D => SyscallCode::SECP256R1_DOUBLE,
             0x00_00_01_2E => SyscallCode::SECP256R1_DECOMPRESS,
+            0x00_00_01_30 => SyscallCode::INNER_PRODUCT
             _ => panic!("invalid syscall number: {value}"),
         }
     }
